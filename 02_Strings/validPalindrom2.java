dc2fabef19142289854c0491ed5f52b6c2bd3661
jatin sood <jatinsood1024@gmail.com>
2024-08-06 23:12:47 +0530

    commit

---
+++ 02_Strings/validPalindrom2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int n = s.length();
+        int i =0;
+        int j = n - 1;
+
+        while(i <= j){
+
+            if(s.charAt(i) != s.charAt(j)){
+                return check(i + 1, j, s) || check(i, j - 1, s);
+            } else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+        
+    }
+
+    boolean check(int i, int j, String s){
+
+        while( i <= j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
+
